01eca040d2f8317970d3bd0265b0a111529a4960
Gonzalo <hgonzalo2000@gmail.com>
2022-05-03 12:49:18 -0300

    chatBot and Simon as Objects

--- chatBot.js
+++ chatBot.js
@@ -1,72 +1,38 @@
 const { Client } = require('whatsapp-web.js');
+const { Simon } = require('./Simon.js');
 const qrcode = require('qrcode-terminal');
-var client;
 
+class ChatBot {
+	constructor() {
+		this.client = new Client();
+		this.simon = new Simon(this);
 
-function responseMessageTable(message) {
-	switch(message.body) {
-		case 'Hola simon':
-			sendResponseMessage(message.from, "Hola soy Simon! Este es un mensaje automatico. Gonzalo te contestara apenas se encuentre disponible");
-	}
-}
+		this.client.on('qr', qr => {
+			qrcode.generate(qr, {small: true});
+		});
 
-// Cuanto queda para vernos, leeme un poema, escribime un poema.
-function responseMessageChiaraTable(message) {
-	switch(message.body) {
-		case 'Hola simon':
-			sendResponseMessage(message.from, "Hola Chiarita. Que queres que te consiga?");
-			break;
+		this.client.on('ready', () => {
+			console.log("Client is ready!");
+		});
 
-		case 'Hola':
-			sendResponseMessage(message.from, "Hola amor, no puede dormir chiqui??");
-			break;
+		this.client.on('message', message => {
+			this.listenMessage(message);
+		})
 
-		case 'Te extraño':
-			sendResponseMessage(message.from, "Yo te extraño mas amor, dentro de poco nos vemos!");
-			break;
-
-		case 'Te amo':
-			sendResponseMessage(message.from, "Yo mas amor, no se presta a discucion...Mucho mucho te amor");
-			break;
-
-		case 'Por igual':
-			sendResponseMessage(message.from, "Bueno por igual te la dejo pasar solo porque sos vos y sos lo mejor que me paso");
-			break;
-
-		default:
-			break;
+		this.client.initialize();
 	}
 
-}
+	sendResponseMessage(receptor, message) {
+		this.client.sendMessage(receptor, message);
+	}
 
-function sendResponseMessage(receptor, message) {
-	client.sendMessage(receptor, message);
-}
-
-function listenMessage() {
-	client.on('message', message => {
-		if (message.from == '5492234973593@c.us') { 
-			responseMessageChiaraTable(message);
-		}
-		else {
-			responseMessageTable(message);
-		}
-	});
+	listenMessage(message) {
+		this.simon.responseMessage(message);
+	}
 }
 
 function chatBot() {
-	client = new Client();
-	
-	client.on('qr', qr => {
-   		qrcode.generate(qr, {small: true});
-	});
-
-	client.on('ready', () => {
-		console.log("Client is ready!");
-		listenMessage();
-	});
-	
-	client.initialize();
+	new ChatBot();
 }
 
 chatBot();
